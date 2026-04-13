75110235f3b6a6bf4e6833bae2d162f0f9e5a4f5
Marvin Francisconi <125971672+francisconimarvin@users.noreply.github.com>
2026-04-13 03:33:25 +0000

    Delete square.py

--- square.py
+++
@@ -1,9 +0,0 @@
-test = []
-
-for i in range(0, int(input()), 1):
-    square = input().split(' ')
-    test.append([int(i) for i in square])
-
-for i in test:
-        if sum(i)/4 == i[0]: print('YES')
-        else: print('NO')
