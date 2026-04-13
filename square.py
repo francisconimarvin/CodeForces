f748479142e4781ab3ba5fbba5707a38f1d64dee
Marvin Francisconi <125971672+francisconimarvin@users.noreply.github.com>
2026-04-13 03:36:38 +0000

    Codeforces Round 1062 (Div. 4) A. Square?

--- square.py
+++ square.py
@@ -7,3 +7,23 @@ for i in range(0, int(input()), 1):
 for i in test:
         if sum(i)/4 == i[0]: print('YES')
         else: print('NO')
+
+
+#INPUT:
+#7
+#1 2 3 4
+#1 1 1 1
+#2 2 2 2
+#1 2 1 2
+#1 1 5 5
+#5 5 5 5
+#4 10 5 9
+
+#OUTPUT:
+#NO
+#YES
+#YES
+#NO
+#NO
+#YES
+#NO
